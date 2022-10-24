667c885f0db50352a8965ade25507ea7e5699e1e
JMaben123 <josh.maben@gmail.com>
2022-10-24 17:11:41 -0400

    ground shake now working kinda
    need to tweak so building doesnt shake off too easily but still moves enough

--- Assets/GroundShake.cs
+++ Assets/GroundShake.cs
@@ -9,11 +9,12 @@ public class GroundShake : MonoBehaviour
     public Rigidbody rb;
     public bool shaking;
     public float time;
+    public float bounding;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 10;
     }
 
     // Update is called once per frame
@@ -22,10 +23,12 @@ public class GroundShake : MonoBehaviour
         if(shaking == true)
         {
             shake(magnitude);
+            rb.isKinematic = false;
         }
         else
         {
-            rb.transform.position = Vector3.Lerp(rb.transform.position, Vector3.zero, time*Time.deltaTime);
+            rb.isKinematic = true;
+            rb.transform.position = Vector3.Lerp(rb.transform.position, new Vector3(0, 0.25f, 0), time*Time.deltaTime);
         }
         //rb.AddForce(transform.right * 2);
     }
@@ -33,10 +36,17 @@ public class GroundShake : MonoBehaviour
 
     void shake(int magnitude)
     {
-        float amount = Random.Range(-0.5f, 0.5f) * magnitude;
-        rb.AddForce(amount, 0, amount);
+        float amount = Random.Range(-10f, 10f);
+        amount = amount * magnitude;
+        //rb.AddForce(0,amount,0);
+        rb.AddForce(amount, amount, amount);
         //rb.transform.position = new Vector3(amount, 0.0f, amount) *Time.deltaTime;
-        Vector3.Lerp(rb.transform.position, Vector3.zero, Time.deltaTime);
+        //rb.transform.Rotate(amount, amount, 0); 
+        if(rb.transform.position.x < -bounding || rb.transform.position.x > bounding || rb.transform.position.z < -bounding || rb.transform.position.z > bounding)
+        {
+            rb.transform.position = Vector3.Lerp(rb.transform.position, new Vector3(0,0.25f,0), time * Time.deltaTime);
+        }
+            
 
         //intensity value controlled by sin curve
     }
