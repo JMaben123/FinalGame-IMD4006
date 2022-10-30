26f5c10002d352b54a70e1a94013cc5b68312fa1
williamt26 <80166033+williamt26@users.noreply.github.com>
2022-10-30 12:51:33 -0400

    Tilt controller fully implemented
    -Tilting is now completely working and functional
    -Script now has an adjustable variable for modifying the speed of the tilt

--- Assets/Scripts/TiltController.cs
+++ Assets/Scripts/TiltController.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 public class TiltController : MonoBehaviour
 {
     //Define variables for the tilt
-    public float rotateValZ = 0f;
-    private float tiltTimer = 0f;
+    public float rotateValZ = 0.0f;
+    private float tiltTimer = 0.0f;
+    public float speedController = 1.5f; //This value controls the speed in buttonTimer()
 
     //This function controls the timer (through Update), and only activates when the player inputs
     void buttonTimer()
     {
-        tiltTimer += Time.deltaTime;
-        Debug.Log("The rotator has been held down for " + tiltTimer + " seconds");
+        tiltTimer += Time.deltaTime * speedController; //This line takes the speedController variable
+        Debug.Log("The rotator has been held down for " + tiltTimer + " seconds"); 
+    }
 
+    //Controls the rotations
+    void rotatorInput()
+    {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            //Activate timer
+            buttonTimer();
             //Rotate z negative
-            rotateValZ = (tiltTimer * -1) * 2;
+            rotateValZ = -tiltTimer * 1.05f;
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
+            //Activate timer
+            buttonTimer();
             //Rotate z positive
-            rotateValZ = (tiltTimer * 2);
+            rotateValZ = tiltTimer * 1.05f;
         }
 
         //If the rotator reaches an extreme at either end
-        else if (rotateValZ >= 45)
+        else if (rotateValZ >= 1)
         {
-            rotateValZ = 45;
+            rotateValZ = 1;
+            //Stop timer
+            tiltTimer = 0;
         }
 
-        else if (rotateValZ <= -45)
+        else if (rotateValZ <= -1)
         {
-            rotateValZ = -45;
+            rotateValZ = -1;
+            //Stop timer
+            tiltTimer = 0;
         }
 
         //If either or button is released
@@ -43,13 +56,13 @@ public class TiltController : MonoBehaviour
             if (rotateValZ > 0)
             {
                 //Rotate z back to 0
-                rotateValZ = (tiltTimer * 2);
+                rotateValZ = (tiltTimer * 0.05f);
             }
 
             if (rotateValZ < 0)
             {
                 //Rotate z back to 0
-                rotateValZ = (tiltTimer * -2);
+                rotateValZ = (tiltTimer * -0.05f);
             }
 
             //When the values get close to 0, snap it to point
@@ -77,7 +90,7 @@ public class TiltController : MonoBehaviour
     void Update()
     {
         //Activate functions
-        buttonTimer();
+        rotatorInput();
         geoTilter();
     }
 }
