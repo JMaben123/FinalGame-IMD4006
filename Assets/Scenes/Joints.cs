116fce1069bd34a9cfe3410dea375121eafa1b41
ProfessionalDylanWay <IMD2006AFORAGEFRENZY@GMAIL.COM>
2022-11-01 22:10:40 -0400

    Joints and Placement
    Added Joint system, and a placement system for the building blocks

---
+++ Assets/Scenes/Joints.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joints : MonoBehaviour
+{
+    float x = 1600f;        //disconnect amount (how hard it can bend)
+
+
+    bool hasJoint;
+    void OnCollisionEnter(Collision collision)
+    {
+        //platform = GameObject.Find("platform");
+
+        if (collision.gameObject.GetComponent<Rigidbody>() != null && !hasJoint)
+        {
+            //gameObject.AddComponent<FixedJoint>().breakForce = 2000;    //How much force is needed to break the joint 
+            //gameObject.AddComponent<FixedJoint>().breakTorque = 2000;   //How much stress is needed to break the joint
+            gameObject.AddComponent<FixedJoint>().breakTorque = x;
+            gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
+            hasJoint = true;
+
+        }
+
+    }
+}
