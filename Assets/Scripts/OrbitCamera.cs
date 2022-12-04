e07fd936d99f6713a2475bbf335e163ac035716d
JMaben123 <josh.maben@gmail.com>
2022-12-04 18:11:51 -0500

    orbit camera update
    movement when placing a block works
    can change amount to move
    need to incoporate the block number global value

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -1,4 +1,4 @@
- using System.Collections;
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
@@ -8,32 +8,46 @@ using Cinemachine;
 
 public class OrbitCamera : MonoBehaviour
 {
-    public float rotateSpeed = 300f;
+    public float rotateSpeed;
     int blockAmount = 1; // replace with global block count
     bool blockPlaced = false;
-    float upAmount;
-    float yVal;
+    public float zoomAmount;
+    float zVal;
+    public CinemachineVirtualCamera vcam;
+    CinemachineTransposer transposer;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        upAmount = 1.5f;
-        yVal = gameObject.GetComponent<Transform>().position.y;
+        //zoomAmount = 0.25f;
+        transposer = vcam.AddCinemachineComponent<CinemachineTransposer>();
+        transposer.m_FollowOffset = new Vector3(0f, 7f, -13f);
     }
 
 
     void moveCamera()
     {
-        if(blockPlaced == true)
-        {
-            gameObject.GetComponent<Transform>().position = new Vector3(0, yVal + upAmount, 0);
-        }
+        zVal = transposer.m_FollowOffset.z;
+        //zVal = vcam.m_Follow.position.z;
+        //zVal = gameObject.GetComponent<Transform>().position.z;
+        //gameObject.GetComponent<Transform>().position = new Vector3(0,0, zVal + upAmount);
+        //transposer.m_FollowOffset = new Vector3(0, 0, zVal + upAmount);
+        //m_FollowOffset = new Vector3(0, 0, zVal + upAmount);
+
+        transposer.m_FollowOffset = new Vector3(0f, transposer.m_FollowOffset.y, zVal - zoomAmount);
+
+        print("added block");
+        print(zVal);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float rotateDir = 0f;
         if(Input.GetKey(KeyCode.LeftBracket))
         {
@@ -43,8 +57,12 @@ public class OrbitCamera : MonoBehaviour
         {
             rotateDir = -0.5f;
         }
-       
 
+        
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            moveCamera();
+        }
 
         transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
     }
