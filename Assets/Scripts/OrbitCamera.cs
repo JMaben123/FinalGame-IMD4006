a48488afee3888002337b109da3bcd24f2a1d350
JMaben123 <josh.maben@gmail.com>
2022-12-06 19:32:17 -0500

    added orbitcamera to main scene

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 using Cinemachine;
 
 
+
+
 public class OrbitCamera : MonoBehaviour
 {
-    public float rotateSpeed = 300f;
-    GlobalDataManager gd;
-    int blockAmount;
     public float rotateSpeed;
     int blockAmount = 1; // replace with global block count
     bool blockPlaced = false;
@@ -22,11 +21,6 @@ public class OrbitCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gd = GlobalDataManager.globalDataManager;
-        upAmount = 1.5f;
-        yVal = gameObject.GetComponent<Transform>().position.y;
-        blockAmount = gd.numBlocks;
-        
         //zoomAmount = 0.25f;
         transposer = vcam.AddCinemachineComponent<CinemachineTransposer>();
         transposer.m_FollowOffset = new Vector3(0f, 7f, -13f);
@@ -35,10 +29,6 @@ public class OrbitCamera : MonoBehaviour
 
     void moveCamera()
     {
-        if(blockAmount > 5)
-        {
-            gameObject.GetComponent<Transform>().position = new Vector3(0, yVal + (blockAmount*upAmount), 0);
-        }
         zVal = transposer.m_FollowOffset.z;
         //zVal = vcam.m_Follow.position.z;
         //zVal = gameObject.GetComponent<Transform>().position.z;
@@ -57,29 +47,23 @@ public class OrbitCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         float rotateDir = 0f;
-        if(Input.GetKey(KeyCode.LeftBracket))
+        if (Input.GetKey(KeyCode.LeftBracket))
         {
             rotateDir = 0.5f;
-            transform.Translate(Vector3.left * Time.deltaTime*rotateSpeed);
         }
         if (Input.GetKey(KeyCode.RightBracket))
         {
             rotateDir = -0.5f;
-            transform.Translate(Vector3.right* rotateSpeed * Time.deltaTime);
         }
 
-        moveCamera();
 
-        if(Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))
         {
             moveCamera();
         }
 
-        gameObject.transform.LookAt(GameObject.Find("CameraPivot").transform);
-
-        //transform.position += new Vector3(rotateDir * rotateSpeed * Time.deltaTime, 0);
-        
+        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
     }
 }
