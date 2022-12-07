0cfb3a198e28624a2be5955fc5519c42090c382d
williamt26 <80166033+williamt26@users.noreply.github.com>
2022-12-07 00:14:49 -0500

    Points Manager Update
    -Now accounts for all materials (lines 20-22)
    -Determines the requirements to purchase and place a block (lines 14-17)
    -UI ID checker that checks where the cursor is on the block selector (line 28)
    -New void "pointChecker" that takes the ID of the slot currently selected in the block selector, and uses a switch system to load the amount of materials needed to purchase the block (lines 47-81)
    -TODO: find where the resources are being stored, and load them into this script (line 84)
    -Shop system implemented that checks whether the player has enough materials to place the block down (lines 87-95)
    -TODO: grab function that places the block and put it in that if statement (line 95)
    -Initiate values now includes wood, iron and stone (lines 125-127)

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is the material and currency/point manager script.
+
+//This script controls how many materials the player currently owns, how many points the player has received from a successful defense,
+//and manages the shopping system.
+
+public class PointManager : MonoBehaviour
+{
+
+    //Define variables for all different building block types
+    int woodReq = 0; //Default material variables
+    int ironReq = 0;
+    int stoneReq = 0;
+    int pointsReq = 0;
+
+    //Define variables for the player's inventory (these values are how much the player has currently in their reserves)
+    int wood = 50;
+    int iron = 50;
+    int stone = 50;
+    //Define points data and value
+    int points = 60;
+    GlobalDataManager globalData;
+
+    //Define the ID that checks what the player's UI cursor is on
+    int uiID = 0; //default - block ID = 0
+
+    //BM points values
+    int numLevels = 0;
+    int ptsPerLevel = 10; //BM: pts per level built
+
+    int lvlsSurvived = 0;
+    int ptsPerLvlSurvive = 20; //BM: pts per level that survives action phase
+
+    int flagHeight = 0;
+    int ptsPerHeight = 10; //BM: pts for the height of the 
+
+    //Define switches
+    bool isBlockPlaced = false;
+    bool hasPointsSent = false; //Have the points been sent?
+    bool canMoveObjects = true; //True by default cause the player should be on build phase. Sets to false once action phase begins.
+
+
+    //Points checker - Checks for the current object in the UI, and loads the required price for each block. Manages purchasing
+    void pointChecker()
+    {
+        switch (uiID)
+        {
+            case 0: //Block in the first slot of the UI roulette requirements
+                woodReq = 10;
+                ironReq = 5;
+                stoneReq = 5;
+                pointsReq = 50;
+                break;
+            case 1: //Block in the second slot
+                woodReq = 15;
+                ironReq = 5;
+                stoneReq = 10;
+                pointsReq = 50;
+                break;
+            case 2: //Third slot, etc.
+                woodReq = 20;
+                ironReq = 10;
+                stoneReq = 15;
+                pointsReq = 100;
+                break;
+            case 3:
+                woodReq = 20;
+                ironReq = 10;
+                stoneReq = 10;
+                pointsReq = 150;
+                break;
+            default: //Slot 1 = default (not sure if I need this)
+                woodReq = 10;
+                ironReq = 5;
+                stoneReq = 5;
+                pointsReq = 50;
+                break;
+        }
+
+        //Check if the player has the necessary materials
+        //TODO - find where the resources are being stored, and load them into this script
+
+        //Does the player have enough wood, iron, stone and points?
+        if ((wood == woodReq) && (iron == ironReq) && (stone == stoneReq) && (points == pointsReq))
+        {
+            //Subtract them from the reserve
+            wood = wood - woodReq;
+            iron = iron - ironReq;
+            stone = stone - stoneReq;
+            points = points - pointsReq;
+            //Place the block
+            //TODO - grab function that places the block and put it here
+        }
+        else
+        {
+            //do nothing, maybe an error message? "You do not have the necessary materials."
+        }
+
+
+    }
+
+    //This function controls the checker, which tells the game how well the player has done in their defense.
+    //This is done by checking the Y value of the "flag" object, which is how high the flag is from touching the ground once the earthquake ends.
+    void winPoints()
+    {
+        //Get the y coordinate of the flag
+        //points = points + (10 * position.y) //(commented out because the flag object is not set up yet)
+    }
+
+    //This function will check if the block is currently being placed
+    void blockPlacer()
+    {
+        //If there is a block being placed, manage that here. The block state on the field itself will be determined in the Game Manager
+
+        //If the block is being moved around on the screen, make it so that no other materials can be interacted with in the menu
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initiate values
+        wood = 50; //Start with 50 of each material
+        iron = 50;
+        stone = 50;
+        points = 60; //BM: start with 60 points to spend on 
+        
+        GlobalDataManager.globalDataManager.playerPts = points;
+        globalData = GlobalDataManager.globalDataManager;
+        numLevels = globalData.numBlocks;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(numLevels < globalData.numBlocks) { 
+            numLevels++; 
+            if (GlobalDataManager.globalDataManager.getGameState() == GlobalDataManager.GameState.buildPhase)
+            {
+                //BM: do a calculation for each level placed
+                if(GlobalDataManager.globalDataManager.playerPts == 0)
+                {
+                    points = ptsPerLevel * numLevels;
+                    /*Debug.Log("pts per lvl: " + ptsPerLevel);
+                    Debug.Log("num lvls" + numLevels);*/
+                }
+                else
+                {
+                    points += ptsPerLevel;
+
+                    //Debug.Log("Points: " + points);
+                }
+            }
+            if((GlobalDataManager.globalDataManager.getGameState() == GlobalDataManager.GameState.actionPhase) && (GlobalDataManager.globalDataManager.phaseEnding))
+            {
+                if (GlobalDataManager.globalDataManager.playerPts == 0)
+                {
+                    points = ptsPerLvlSurvive * lvlsSurvived;
+                }
+                else
+                {
+                    points += ptsPerLvlSurvive * lvlsSurvived;
+                    points += flagHeight * ptsPerHeight;
+                }
+            }
+        }
+
+        //**Pseudocode**
+        //Add once game manager adds the Earthquake timer
+        //When the earthquake timer is over and build phase begins, the points will be distributed here
+
+        //if (hasPointsSent == false && earthquakeTimer < 0 && flagPosition.y > 0)
+        //{{
+        //winPoints();
+        //Set the flag to true so that the game isn't constantly adding points to the player
+        hasPointsSent = true;
+        //}}
+
+        GlobalDataManager.globalDataManager.playerPts = points;
+        EventSystem.Instance.changePoints(points);
+        //Debug.Log(points);
+    }
+
+
+    
+}
